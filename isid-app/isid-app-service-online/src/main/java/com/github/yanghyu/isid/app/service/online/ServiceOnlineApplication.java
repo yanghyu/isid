c80741eb0097b05c60ffead28e981e021b6d548c
杨鸿毓 <869752070@qq.com>
2023-03-14 11:32:54 +0800

    20230310 调整模块配置

--- isid-app/isid-app-service-online/src/main/java/com/github/yanghyu/isid/app/service/online/ServiceOnlineApplication.java
+++ isid-app/isid-app-service-online/src/main/java/com/github/yanghyu/isid/app/service/online/ServiceOnlineApplication.java
@@ -1,8 +1,11 @@
 package com.github.yanghyu.isid.app.service.online;
 
+import com.github.yanghyu.isid.web.autoconfigure.tomcat.WebTomcatConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import({WebTomcatConfig.class})
 @SpringBootApplication
 public class ServiceOnlineApplication {
 
